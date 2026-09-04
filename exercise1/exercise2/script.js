de9f30e262c80e8e00782c060ca03c33f8dcba1b
rukiaabdi <ruuneeyqali11@gmail.com>
2026-09-04 07:13:02 -0700

    submited exercise2

---
+++ exercise1/exercise2/script.js
@@ -0,0 +1,19 @@
+let a= 15;
+let b= "15";
+
+// Equal to
+console.log(a == b);
+// Not equal to
+console.log(a !=b);
+// Strictly equal to
+console.log(a === b);
+// Strictly not equal to
+console.log(a !== b);
+// Greater than
+console.log(a > b);
+// Less than
+console.log(a < b);
+// Greater than or equal to 
+console.log(a >= b );
+// Less than or equal to 
+console.log(a <= b );
