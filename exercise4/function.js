a9ac0b68c4379a83131b12b540e6ef6fc205a5f5
rukiaabdi <ruuneeyqali11@gmail.com>
2026-09-05 21:29:36 -0700

    submited exrcise4

---
+++ exercise4/function.js
@@ -0,0 +1,19 @@
+// function add(x,y){
+//     return x+y;
+// };
+
+// console.log(add(9,5));
+// console.log(add(3,7));
+// console.log(add(60,70));
+// console.log(add(10,20));
+
+
+// function expression
+const add= function(x,y){
+    return x + y;
+    
+};
+console.log (add(5,8));
+console.log (add(2,6));
+console.log (add(1,3));
+console.log (add(33,2));
