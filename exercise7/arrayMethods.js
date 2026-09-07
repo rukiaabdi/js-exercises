32a23baa687f16a7886bcae549996f275bf301a4
rukiaabdi <ruuneeyqali11@gmail.com>
2026-09-07 06:57:34 -0700

    submited exercise7

---
+++ exercise7/arrayMethods.js
@@ -0,0 +1,12 @@
+let colors = ["red", "green", "blue"]
+
+console.log(colors);
+
+// add to the end color
+colors.push("yellow");
+console.log(colors);
+
+// remove first color
+colors.shift();
+console.log(colors);
+
