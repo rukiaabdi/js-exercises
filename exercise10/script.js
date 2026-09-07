a2256d5ae268c14b55a05604240615c328275600
rukiaabdi <ruuneeyqali11@gmail.com>
2026-09-07 08:30:55 -0700

    exercise10

---
+++ exercise10/script.js
@@ -0,0 +1,10 @@
+let library = [
+    { name: "National Library" , Title : "The Alchemist" , Author: "Paulo Coelho", Year: 1988},
+    { name: "Modern Library",Title: "Atomic Habits",  Author: "James Clear",Year: 2018},
+    { name: "University Library",Title: "Clean Code",  Author: "Robert C. Martin", Year: 2008}
+];
+
+console.log(library[0].name); 
+console.log(library[1].Title); 
+console.log(library[2].Author);
+console.log(library[2].Year);
