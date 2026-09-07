b26f4f255f381a7d719b353498444b2fbc3835c9
rukiaabdi <ruuneeyqali11@gmail.com>
2026-09-07 07:55:33 -0700

    submited exercise9

---
+++ exercise9/methodObject.js
@@ -0,0 +1,11 @@
+let car ={
+    make: "toyota",
+    model:"camry",
+    year: 2020 ,
+    startCar : function() {
+        console.log("the car has started");
+        
+    }
+} ;
+ 
+car.startCar();
