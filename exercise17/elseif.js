557ef21a26e1f7dd5192416ee274d501216ca3d0
rukiaabdi <ruuneeyqali11@gmail.com>
2026-09-11 20:37:32 -0700

    submited exercise17

---
+++ exercise17/elseif.js
@@ -0,0 +1,11 @@
+let temperature = 10;
+
+if (temperature < 0) {
+    console.log("Very cold");
+} else if (temperature <= 15) {
+    console.log("Cold");
+} else if (temperature <= 25) {
+    console.log("Warm");
+} else {
+    console.log("Hot");
+}
